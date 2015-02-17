2a64a405661b49e0b0db51863e66e017f6315a76
Sam Whitley <sam@samwhitley.name>
2015-02-16 21:09:37 -0600

    Add botanical background

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: "dev/scss/*.scss",
+        tasks: "sass:dev"
+      },
+      css : {
+        files: "dev/css/*.css",
+        tasks: "cssmin"
+      },
+      js : {
+        files: "dev/js/*.js",
+        tasks: "uglify"
+      }
+    },
+    sass: {
+      dev: {
+        files: {
+          "dev/css/styles.css": "dev/scss/styles.scss"
+        }
+      }
+    },
+    uglify: {
+      dev: {
+        files: [
+          {
+            src: 'dev/js/main.js',
+            dest: 'build/js/min.js'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/css/min.css': ['dev/css/reset.css', 'dev/css/styles.css']
+        }
+      }
+    },
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "build/css/*.css",
+            "build/*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "build/"
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
